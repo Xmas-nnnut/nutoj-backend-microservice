b2dd9f4c3f2d677b8cd2d4b562ae738d75ee8b92
xuqingjian <xuqingjian894@gmail.com>
2023-09-17 21:21:22 +0800

    fix: 服务内部调用

--- nutoj-backend-gateway/src/main/java/com/xqj/nutojbackendgateway/NutojBackendGatewayApplication.java
+++ nutoj-backend-gateway/src/main/java/com/xqj/nutojbackendgateway/NutojBackendGatewayApplication.java
@@ -2,8 +2,10 @@ package com.xqj.nutojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NutojBackendGatewayApplication {
 
     public static void main(String[] args) {
--- nutoj-backend-question-service/src/main/java/com/xqj/nutojbackendquestionservice/NutojBackendQuestionServiceApplication.java
+++ nutoj-backend-question-service/src/main/java/com/xqj/nutojbackendquestionservice/NutojBackendQuestionServiceApplication.java
@@ -3,6 +3,8 @@ package com.xqj.nutojbackendquestionservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @ComponentScan("com.xqj")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.xqj.nutojbackendserviceclient.service"})
 public class NutojBackendQuestionServiceApplication {
 
     public static void main(String[] args) {
