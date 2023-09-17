e8484bb3f9d55c543db629d8911fb29b00c89833
xuqingjian <xuqingjian894@gmail.com>
2023-09-17 18:54:18 +0800

    feat： 用户服务完善

---
+++ nutoj-backend-user-service/src/main/java/com/xqj/nutojbackenduserservice/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xqj.nutojbackenduserservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xqj.nutojbackendmodel.model.entity.User;
+
+/**
+ * 用户数据库操作
+ * @description 针对表【user(用户)】的数据库操作Mapper
+ * @createDate 2023-08-09 15:49:27
+ * @Entity com.xqj.nutoj.model.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
